316f1de4f2d3485454ff2f36235fed1f41d9b3ab
ShiganLuo <2530320102@qq.com>
2026-08-27 15:06:46 +0800

    fix: 前台main.ts补全app.use(ElementPlus)修复空白页面

--- bioplatform-vue3/bioplatform-front/src/main.ts
+++ bioplatform-vue3/bioplatform-front/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import router from './router'
@@ -16,6 +17,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
+app.use(ElementPlus)
 app.use(pinia)
 app.use(router)
 
